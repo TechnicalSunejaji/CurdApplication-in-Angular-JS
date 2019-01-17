3a8042f41825b22e33f4e99948a344b1fc4ca959
burhan5246 <burhanuddinsarangpurwala@gmail.com>
2019-01-17 16:12:31 +0530

    updated app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,12 @@
 var myApp = angular.module('myApp', []);
 
 myApp.controller('myController', function($scope){
-	console.log("In myContoller...");
+	//console.log("In myContoller...");
 	
 	$scope.newUser = {};
 	$scope.checkedUser = {};
 	$scope.message = "";
-
+	$scope.clickedUser ={};
 	$scope.users = 
 	[
 		{FNAME:"Nikhil",LNAME:"Singh",EMAIL:"nikhilsingh2311@gmail.com"}
@@ -17,13 +17,18 @@ myApp.controller('myController', function($scope){
 		$scope.message = "User added Successful";
 	};
 
-	$scope.selectUser = function(user){
-		console.log(user);
-		$scope.clickedUser = user;
+	$scope.selectUser = function(user, index){
+		console.log(index);
+		//$scope.clickedUser = user;
+		$scope.index_key = index;
+		angular.copy(user, $scope.clickedUser);
 	};
 
-	$scope.updateUser = function(user){
-
+	$scope.updateUser = function(updated_user){
+		//console.log(user);
+		$scope.users[$scope.index_key] = updated_user;
+		$scope.index_key ="";
+		$scope.clickedUser = {};
 	};
 
 	$scope.deleteUser = function(){
